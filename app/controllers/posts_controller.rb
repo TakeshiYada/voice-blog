c1b93e83e0511b9f4143332171ffd165eec312da
taker667 <blessing.1016.action@gmail.com>
2020-09-26 14:58:13 +0900

    Implementation of PostSystem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,23 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC")
   end
 
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:audio,:image,:title,:text)
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user, optional: true
+
+  validates :audio, presence: true
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :text, presence: true
 end
