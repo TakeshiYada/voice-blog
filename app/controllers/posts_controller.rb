1f7895fa190a05ecbdfae5d3b2adb8298dcbeaa7
TakeshiYada <blessing.1016.action@gmail.com>
2020-09-30 20:38:15 +0900

    update files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,7 +35,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:audio,:image,:title,:text)
+    params.require(:post).permit(:audio,:image,:title,:text).merge(user_id: current_user.id)
   end
 
   def move_to_index
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  belongs_to :user, optional: true
-  has_many :comments
+  belongs_to :user
+  has_many :comments, foreign_key: :post_id, dependent: :destroy
 
   validates :audio, presence: true
   validates :image, presence: true
